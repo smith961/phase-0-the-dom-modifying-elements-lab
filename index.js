49e71d0c78850495e38bc548b02685a0eead240b
unknown <smithbabawande@gmail.com>
2023-07-27 10:15:20 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('main').remove();
+
+
+ const newHeader = document.createElement("h1");
+  const yourName = "YOUR-NAME"
+  newHeader.id = "victory";
+  newHeader.textContent = `${yourName} is the champion`
+
